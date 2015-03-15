b092a52bde49a49b7dcc1d7430abdb376e49cdaf
eggyolkmedia <y.git@eggyolkmedia.com>
2015-03-15 21:32:36 +0200

    basic functionality, listening to playback events

--- service.py
+++ service.py
@@ -1,12 +1,56 @@
 import time
+import sys
 import xbmc
- 
+import xbmcaddon
+
+class LightMonitor(xbmc.Player):
+
+  def __init__(self):
+    xbmc.Player.__init__(self)
+    addon = xbmcaddon.Addon()
+    self.server_host = addon.getSetting('server-host')
+    self.server_port = addon.getSetting('server-port')
+    self.media_types = addon.getSetting('media-types').split(',')
+
+    log("Starting; host={}, port={}, media types={}".format(self.server_host, self.server_port, self.media_types))
+
+  def onPlayBackStarted(self):
+    log("started")
+    self.mediaStarted()
+
+  def onPlayBackResumed(self):
+    log("resumed")
+    self.mediaStarted()
+
+  def onPlayBackStopped(self):
+    log("stopped")
+    self.mediaStopped()
+
+  def onPlayBackPaused(self):
+    log("paused")
+    self.mediaStopped()
+
+  def onPlayBackEnded(self):
+    log("ended")
+    self.mediaStopped()
+
+  def mediaStarted(self):
+    # log("Playback started")
+    log("video? {}".format(self.isPlayingVideo()))
+
+  def mediaStopped(self):
+    # log("Playback stopped")
+    pass
+
+def log(msg):
+  xbmc.log('[ZL] ' + msg, level=xbmc.LOGDEBUG)
+
 if __name__ == '__main__':
-    monitor = xbmc.Monitor()
- 
-    while True:
-        # Sleep/wait for abort for 10 seconds
-        if monitor.waitForAbort(10):
-            # Abort was requested while waiting. We should exit
-            break
-        xbmc.log("ZL TEST %s" % time.time(), level=xbmc.LOGDEBUG)
+  monitor = LightMonitor()
+
+  while not xbmc.abortRequested:
+    xbmc.sleep(500)
+
+  log("Shutting down")
+  sys.exit(0)
+
